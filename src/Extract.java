c42bdd35f47cc469ba2c87cd7529f18147f70cf0
Md Johirul Islam <johirbuet@gmail.com>
2018-05-24 09:48:05 -0500

    done

--- src/Extract.java
+++ src/Extract.java
@@ -1,4 +1,39 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Extract {
+	public static void main(String[] args) {
+		String path = "/Users/mislam/Desktop/ICSE/score5/sklearn/sklearn.csv";
+        BufferedReader br = null;
+        String line = "";
+        String cvsSplitBy = ",";
 
+        try {
+
+            br = new BufferedReader(new FileReader(path));
+            while ((line = br.readLine()) != null) {
+
+                // use comma as separator
+                String[] columns = line.split(cvsSplitBy);
+                
+                System.out.println(columns.length);
+
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+	}
 }
---
+++ src/Post.java
@@ -0,0 +1,7 @@
+
+public class Post {
+	enum QTYPE {ANSWER,QUESTION};
+	public int id;
+	public String title;
+	public Body body;
+}
